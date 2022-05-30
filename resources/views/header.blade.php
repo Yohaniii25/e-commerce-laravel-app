1bc8c4b6fcc646f42ca696faa8f3510c746f1965
Yohaniii25 <yohanii725@gmail.com>
2022-05-30 09:42:44 +0530

    added some adjustments

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,8 +17,8 @@
                 </li>
             </ul>
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control rounded" style=" margin-top:2px; margin-left: 436px;" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-primary" style="margin-left:10px;" type="submit">Search</button>
             </form>
         </div>
     </div>
